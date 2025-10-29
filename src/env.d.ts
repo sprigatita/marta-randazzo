4ad95b8e60fd9b77eb919e4a61bb5d34b0effdfd
marta <randazzomarta@icloud.com>
2025-10-29 22:42:35 +0100

    non ha senso

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_GITHUB_TOKEN?: string;
+    readonly VITE_GITHUB_TOKEN?: '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';
 }
 
 interface ImportMeta {
